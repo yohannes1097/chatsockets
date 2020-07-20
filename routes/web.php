7729b9bd6e9cf9cb770d875747927b28473322d7
Yohannes1097 <yohannes10.97@gmail.com>
2020-07-21 06:36:00 +0700

    Forgot to add Chat Model

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/rooms', 'RoomController@index')->middleware('auth');
 Route::get('/room/{id}', 'RoomController@room')->middleware('auth');
 Route::get('/chat/get_chat/{room_id}', 'ChatController@getChat')->middleware('auth');
 Route::post('/chat/post_chat/{room_id}', 'ChatController@postChat')->middleware('auth');
+Route::post('/rooms/new', 'RoomController@new')->middleware('auth');
 
 Auth::routes();
 
